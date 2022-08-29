525fea707adfb0013ede65968606b4a6b84da3c5
nkpe <naomi.pearse@gmail.com>
2022-08-29 13:27:24 +0100

    Header added

--- main-app/src/index.js
+++ main-app/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Header from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <Header />
 );
 
 // If you want to start measuring performance in your app, pass a function
